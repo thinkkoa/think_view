3ddbb936f668c020acd13cfdfc3ea9a241fb4998
yanglin <richenlin@gmail.com>
2017-06-12 11:55:53 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# think_view
-View engine for ThinkKoa
+# 介绍
+-----
+
+[![npm version](https://badge.fury.io/js/think_view.svg)](https://badge.fury.io/js/think_view)
+[![Dependency Status](https://david-dm.org/richenlin/think_view.svg)](https://david-dm.org/richenlin/think_view)
+
+View engine for ThinkKoa.
+
+# 安装
+-----
+
+```
+npm i think_view
+```
+
+# 使用
+-----
+
+1、项目中增加中间件 middleware/view.js
+```
+module.exports = require('think_view');
+```
+
+2、项目中间件配置 config/middleware.js:
+```
+list: [...,'view'], //加载的中间件列表
+config: { //中间件配置
+    ...,
+    view: {
+        engine_type: 'ejs', //模版引擎名称
+        engine_config: { cache: true }, //模版引擎配置
+        content_type: 'text/html', //模版输出类型
+        file_suffix: '.html', //模版文件名后缀
+        file_depr: '_', //controller和action之间的分隔符
+        default_theme: 'default', //默认模板主题
+    }
+}
+```
---
+++ index.js
@@ -0,0 +1,87 @@
+/**
+ *
+ * @author     richen
+ * @copyright  Copyright (c) 2017 - <richenlin(at)gmail.com>
+ * @license    MIT
+ * @version    17/6/12
+ */
+const lib = require('think_lib');
+
+/**
+ * 
+ * 
+ * @param {any} templateFile 
+ * @param {any} ctx 
+ * @param {any} options 
+ * @returns 
+ */
+const locateTpl = function (templateFile, ctx, options) {
+    let group = ctx.group ? ctx.group : '',
+        controller = ctx.controller ? ctx.controller : '',
+        action = ctx.action ? ctx.action : '',
+        app_path = options.app_path ? options.app_path : process.cwd();
+
+    if (lib.isEmpty(templateFile)) {
+        templateFile = [app_path, lib.sep, 'view', lib.sep];
+        if (group) {
+            templateFile.push(group);
+            templateFile.push(options.sep);
+        }
+        templateFile.push(options.default_theme || 'default');
+        templateFile.push(options.sep);
+
+        templateFile.push(controller);
+        templateFile.push(options.file_depr || '_');
+
+        templateFile.push(action);
+        templateFile.push(options.file_suffix || '.html');
+        return templateFile.join('');
+    } else if(lib.isString(templateFile)){
+        if (lib.isFile(templateFile)) {
+            return templateFile;
+        }
+        let tplPath = templateFile.split('/');
+        action = tplPath.pop().toLowerCase() || action;
+        controller = tplPath.pop().toLowerCase() || controller;
+        group = tplPath.pop();
+        templateFile = [app_path, lib.sep, 'view', lib.sep];
+        if (group) {
+            templateFile.push(group.toLowerCase());
+            templateFile.push(options.sep);
+        }
+        templateFile.push(options.default_theme || 'default');
+        templateFile.push(options.sep);
+
+        templateFile.push(controller);
+        templateFile.push(options.file_depr || '_');
+
+        templateFile.push(action);
+        templateFile.push(options.file_suffix || '.html');
+        return templateFile.join('');
+    }
+    return null;
+};
+
+
+module.exports = function (options) {
+    think.app.once('appReady', () => {
+        const engine = require(`./lib/adapter/${options.engine_type || 'ejs'}`);
+        think._view = new engine(options.engine_config || {});
+    });
+
+    return function (ctx, next) {
+        ctx.render = function (templateFile, data, charset, contentType) {
+            let tplFile = locateTpl(templateFile, ctx, options);
+            if (!tplFile || !lib.isFile(tplFile)) {
+                ctx.throw(404, `can\'t find template file ${tplFile || ''}`);
+            }
+            charset = charset || think._caches.configs.config['encoding'] || 'utf-8';
+            contentType = contentType || 'text/html';
+            ctx.types(contentType, charset);
+            return think._view.fatch(tplFile, data).then(res => {
+                ctx.body = res;
+            });
+        };
+        return next();
+    };
+};
